ff85fb6b065ff87292a142c270be7430fb152525
s1215616 <s1215616@student.windesheim.nl>
2026-04-21 15:33:51 +0200

    added user/tenant detection and having user info in blade

---
+++ Autovoorraad/resources/views/public_website/index.blade.php
@@ -0,0 +1,2 @@
+<img src="{{ asset('storage/' . $tenant->herofoto) }}" alt="Logo" class="w-32 h-32 object-contain mb-4">
+{{ dd($tenant) }}
--- Autovoorraad/routes/web.php
+++ Autovoorraad/routes/web.php
@@ -4,22 +4,24 @@ use App\Http\Controllers\AutoAPIController;
 use App\Http\Controllers\AutoAPIGetController;
 use App\Http\Controllers\AutoController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\PublicWebsiteController;
 use App\Http\Controllers\WebsiteSettingsController;
+use App\Http\Middleware\SubdomainTenantDetection;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 Route::domain('{tenant}.'.parse_url(config('app.url'), PHP_URL_HOST))->group(function () {
-    Route::get('/', function ($tenant) {
-        return 'Tenant: '.$tenant;
-    });
+    Route::get('/', [PublicWebsiteController::class, 'index'])->middleware(SubdomainTenantDetection::class);
 });
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('/dashboard', function () {
         return view('dashboard');
-    })->name('dashboard');
+    })
+    ->name('dashboard');
     
     Route::get('/auto-toevoegen', [AutoController::class, 'AutoToevoegen'])->name('auto.toevoegen');
     Route::get('/auto/{id}', [AutoController::class, 'details'])->name('auto.detail');
