32d0e653daabd0693d9afcb24c5ea8e666e06f6a
s1215616 <s1215616@student.windesheim.nl>
2026-04-23 23:51:15 +0200

    quick fix

--- Autovoorraad/resources/views/public_website/index.blade.php
+++ Autovoorraad/resources/views/public_website/index.blade.php
@@ -91,7 +91,7 @@
                     <svg xmlns="http://www.w3.org/2000/svg" class="text-[var(--primary_color)]" width="18" height="18" viewBox="0 0 18 18" fill="none">
                     <path d="M8 18V12H10V14H18V16H10V18H8ZM0 16V14H6V16H0ZM4 12V10H0V8H4V6H6V12H4ZM8 10V8H18V10H8ZM12 6V0H14V2H18V4H14V6H12ZM0 4V2H10V4H0Z" fill="currentcolor"/>
                     </svg>
-                    <h2 class="inline text-2xl font-semibold font-serif uppercase">Filters</h2>
+                    <h2 class="inline text-2xl  font-semibold font-serif uppercase">Filters</h2>
                 </div>   
                 <div class="flex w-full gap-4 flex-col md:flex-row">
                     <div class="w-1/4">
